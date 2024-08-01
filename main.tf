d75ff8841622b26bf156cae02d9607d14dbfe2ca
Gilles Flisch <info@arc4u.net>
2024-08-01 14:23:49 +0200

    Configured Dns (not yet prod ready) Add certificate

--- main.tf
+++ main.tf
@@ -35,9 +35,33 @@ resource "azurerm_container_app_environment" "my_app" {
   tags = local.default_tags
 }
 
+resource "azurerm_dns_zone" "dns" {
+  name                = "acceptancetest.arc4u.net"
+  resource_group_name = azurerm_resource_group.my_app.name
+}
+
+resource "azurerm_dns_txt_record" "dns" {
+  name                = "asuid.acceptancetest.arc4u.net"
+  zone_name           = azurerm_dns_zone.dns.name
+  resource_group_name = azurerm_resource_group.my_app.name
+  ttl                 = 300
+
+  record {
+    value = "8224261506781F996518E27935085891A4429CCE65D1EFD2EA39FA68B00F83E8"
+  }
+}
+
+resource "azurerm_container_app_environment_custom_domain" "arc4u_net" {
+  container_app_environment_id = azurerm_container_app_environment.my_app.id
+  certificate_blob_base64 = filebase64("${path.module}/certs/arc4u.net.pfx")
+  certificate_password = var.cert_password
+  dns_suffix = "acceptancetest.arc4u.net"
+}
+
 resource "azurerm_container_app_environment_certificate" "arc4u_net" {
   name = "cert-arc4u.net"
   certificate_blob_base64 = filebase64("${path.module}/certs/arc4u.net.pfx")
   certificate_password = var.cert_password
   container_app_environment_id = azurerm_container_app_environment.my_app.id
 }
+
